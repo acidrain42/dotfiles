b79a25b14a377f18fcf0623edaca6c2326dc63f6
Mathieu Lemay <acidrain1@gmail.com>
2019-01-10 13:43:00 -0500

    ycm.conf.py: Use /usr/local as prefix fallback

--- ycm.conf.py
+++ ycm.conf.py
@@ -2,7 +2,7 @@ import os
 
 
 def Settings(**kwargs):
-    prefix = os.environ.get('VIRTUAL_ENV', '/usr')
+    prefix = os.environ.get('VIRTUAL_ENV', os.path.join('/usr', 'local'))
     return {
         'interpreter_path': os.path.join(prefix, 'bin', 'python')
     }
